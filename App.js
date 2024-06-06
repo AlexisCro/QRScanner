697141524fa43ad2e13c4bceaca5e1b038694047
mdeport <mateo.deport@ecole-isitech.fr>
2024-06-06 15:02:44 +0200

    stop camera

--- App.js
+++ App.js
@@ -1,51 +1,61 @@
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  const [facing, setFacing] = useState('back');
+  const [facing, setFacing] = useState("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const [cameraActive, setCameraActive] = useState(true);
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Text style={{ textAlign: "center" }}>We need your permission to show the camera</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
   function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+    setFacing((current) => (current === "back" ? "front" : "back"));
   }
 
   function displayResultBarCode(e) {
     alert(e.data);
-    stopRecording()
+    console.log(e.data);
+    setCameraActive(false);
+  }
+
+  function rescanQR() {
+    setCameraActive(true);
   }
 
   return (
     <View style={styles.container}>
-      <CameraView 
-        style={styles.camera}
-        facing={facing}
-        barcodeSettings={{
-          barcodeTypes: ["qr"],
-
-        }}
-        onBarcodeScanned={(e) => {displayResultBarCode(e.)}}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </CameraView>
+      {cameraActive ? (
+        <CameraView
+          style={styles.camera}
+          facing={facing}
+          barcodeSettings={{
+            barcodeTypes: ["qr"],
+          }}
+          onBarcodeScanned={(e) => {
+            displayResultBarCode(e);
+          }}
+        >
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      ) : (
+        <Button onPress={rescanQR} title="Rescanner un QR code" />
+      )}
     </View>
   );
 }
@@ -53,25 +63,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   camera: {
     flex: 1,
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
+    flexDirection: "row",
+    backgroundColor: "transparent",
     margin: 64,
   },
   button: {
     flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignSelf: "flex-end",
+    alignItems: "center",
   },
   text: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+    fontWeight: "bold",
+    color: "white",
   },
 });
